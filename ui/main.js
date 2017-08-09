9a02d386f6305b1ff0238b973428fb3928afdb1d
lucky123456789 <prakash0544@gmail.com>
2017-08-09 10:10:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,15 +3,15 @@ var button=document.getElementById('counter');
 button.onclick = function () {
     
     //Create Request Object
-    var request=new XMLHttpRequest();
+    var request = new XMLHttpRequest();
     
     //Capture the response and store it is a variable
-    request.onreadystatechange=function(){
+    request.onreadystatechange = function(){
         if(request.readyState === XMLHttpRequest.DONE){
             
             //Take SOme action
             if(request.status === 200){
-             var counter =request.responseText;
+             var counter = request.responseText;
              var span = document.getElementById('count');
              span.innerHTML =counter.toString();
             }
@@ -19,6 +19,6 @@ button.onclick = function () {
         //not done yet
     }
     //make the request
-    request.open('GET','http://prakash0544.imad.hasura-app.io/counter',true);
+    request.open('GET','http://prakash0544.imad.hasura-app.io/counter', true);
     request.send(null);
 }
